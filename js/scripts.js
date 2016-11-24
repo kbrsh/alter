ed5d5d536127bbc97cbabf8fae8381388b014e3f
Gopal Shah <simplecooldude1@gmail.com>
2016-11-24 15:27:49 -0800

    generate function

--- js/scripts.js
+++ js/scripts.js
@@ -2,25 +2,27 @@ var canvas = document.getElementById('canv'),
 		ctx = canvas.getContext('2d'),
     img = document.getElementById('image');
 
+var generate = function() {
+	var text = document.getElementById('text').value.split("\n").join("\n");
+	var x = 12.5;
+	var y = 15;
+	var lineheight = 30;
+	var lines = text.split('\n');
+	var lineLengthOrder = lines.slice(0).sort(function(a, b) {
+		return b.length - a.length;
+	});
+	ctx.canvas.width = ctx.measureText(lineLengthOrder[0]).width + 25;
+	ctx.canvas.height = (lines.length * lineheight);
 
+	ctx.fillStyle = "#232323";
+	ctx.fillRect(0, 0, canvas.width, canvas.height);
+	ctx.textBaseline = "middle";
+	ctx.font="20px Anonymous Pro";
+	ctx.fillStyle = "#BBBBBB";
+	for (var i = 0; i<lines.length; i++)
+		ctx.fillText(lines[i], x, y + (i*lineheight) );
+	img.src = ctx.canvas.toDataURL();
+}
 document.getElementById('submit').addEventListener('click', function (){
-		var text = document.getElementById('text').value.split("\n").join("\n");
-    var x = 12.5;
-		var y = 15;
-		var lineheight = 30;
-		var lines = text.split('\n');
-    var lineLengthOrder = lines.slice(0).sort(function(a, b) {
-    	return b.length - a.length;
-    });
-    ctx.canvas.width = ctx.measureText(lineLengthOrder[0]).width + 25;
-    ctx.canvas.height = (lines.length * lineheight);
-
-    ctx.fillStyle = "#232323";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    ctx.textBaseline = "middle";
-    ctx.font="20px Anonymous Pro";
-		ctx.fillStyle = "#BBBBBB";
-		for (var i = 0; i<lines.length; i++)
-    	ctx.fillText(lines[i], x, y + (i*lineheight) );
-    img.src = ctx.canvas.toDataURL();
+	
 });
